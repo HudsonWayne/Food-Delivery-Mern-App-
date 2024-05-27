e03fadbb5c46b34b7f14018902b26c5218902bdd
HudsonWayne <waynewaynebenhura@gmail.com>
2024-05-27 11:29:55 +0200

    connecting  mongoDB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,33 @@
-import mongoose from "mongoose";
+// const { MongoClient, ServerApiVersion } = require('mongodb');
 
-// db connection
-// Database Connection
-mongoose.connect("mongodb+srv://hudsonnbenhuraa:<password>@cluster0.5nrizx2.mongodb.net/food-del")
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.error("DB Connection Error:", err));
+// const uri = "mongodb+srv://hudsonnbenhuraa:<password>@cluster0.5nrizx2.mongodb.net/food-del"; // Remove the port number
+
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// mongodb+srv:hudsonnbenhuraa:<password>@cluster0.uigojht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+
+
+import mongoose from 'mongoose';
+
+const connectDB = async () => {
+  try {
+    // Use 'await' keyword before mongoose.connect()
+    const conn = await mongoose.connect('mongodb+srv://hudsonnbenhuraa:<password>@cluster0.uigojht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`); // Template literal for string formatting
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -10,10 +10,6 @@ const port = 4000;
 app.use(express.json());
 app.use(cors());
 
-// Database Connection
-mongoose.connect("mongodb+srv://hudsonnbenhuraa:<1234>ping _mongodb._tcp.luster0.5nrizx2.mongodb.net/food-del")
-  .then(() => console.log("DB Connected"))
-  .catch((err) => console.error("DB Connection Error:", err));
 
 // Routes
 app.get("/", (req, res) => {
@@ -24,3 +20,4 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Server started on http://localhost:${port}`);
 });
+
