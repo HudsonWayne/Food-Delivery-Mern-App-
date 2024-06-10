fed23f7808298a794247321c1152b6909bcd4554
HudsonWayne <waynewaynebenhura@gmail.com>
2024-06-10 15:27:24 +0200

    added food

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -6,6 +6,20 @@ import fs from 'fs'
 
 const addFood = async (res,req) => {
 
+    let image_filename = `${req.file.filename}`;
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+    try{
+        await food.save();
+        res.json({success:true, message: "Food Added"})
+    } catch (error){
+
+    }
 }
 
 export{addFood}
