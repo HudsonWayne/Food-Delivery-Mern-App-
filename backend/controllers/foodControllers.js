16beae5bd1c6c9ba598eed12e2c5d98fe47ccd98
HudsonWayne <waynewaynebenhura@gmail.com>
2024-06-17 10:58:42 +0200

    added a food remover router, that when its not added it must provide an error

--- backend/controllers/foodControllers.js
+++ backend/controllers/foodControllers.js
@@ -62,9 +62,16 @@ const listFood = async (req, res) => {
 
 //remove food item
 const removeFood = async (req, res) => {
-  // try {
-  //   const id = req.params.id;
-  // }
+  try {
+    const food = await foodModel.findById(req.body.id);
+    fs.unlink(`uploads/${food.image}`,()=>{})
+
+    await foodModel.findByIdAndDelete(req.body.id);
+    res.json({success:true,message:"Food Removed"})
+  }catch(error){
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
 }
 
 export { addFood, listFood, removeFood};
